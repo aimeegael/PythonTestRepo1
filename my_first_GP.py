2d5322b195049cf277e27d19bd944624b46322c9
Ahad Shaikh <ahada@hotmail.com>
2020-12-04 18:45:45 -0500

    merged and added new code

--- my_first_GP.py
+++ my_first_GP.py
@@ -3,10 +3,13 @@ def print_hello(input_string):
 
 print_hello("Abdul Ahad")
 
+def print_banner(new_input):
+    print(f"This is a test for banner {new_input}")
+
 msg1 = "hello there"
 
 print(msg1.upper())
 
 newArray = [1,2,3,4,5]
 for item in newArray:
-    print_hello(item)
+    print_banner(item)
